a4c4277ab3c5b1c51c4f460fe2641cfb94e8aa75
Valentin Leonard--bertrand <lnv5091a@ensfsi.onmicrosoft.com>
2025-03-20 19:19:44 +0100

    fin tp3

--- src/controleur/ControlPrendreEtal.java
+++ src/controleur/ControlPrendreEtal.java
@@ -18,8 +18,8 @@ public class ControlPrendreEtal {
 	}
 
 	public int prendreEtal(String nomVendeur, String produit, int nbProduit) {
-		//TODO a completer
 		int numeroEtal = -1;
+		verifierIdentite(nomVendeur);
 		return numeroEtal;
 	}
 
